9afe2325e284c424aaa5c47ef6b9f4bd7c6f7345
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-03-27 20:06:39 -0300

    atualizado

--- main1.cpp
+++ main1.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
+#include<iomanip>
 
 int main(){
 
-    std::string saudacao = "Boa noite, Eduardo ";
-    //comentário de teste
-    std::cout<<saudacao<<std::endl;
+int a = 8;
+float b = 9.6;
+double c = 8.695136784;
+
+std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
+
+std::cout<<a<<std::endl;
+std::cout<<b<<std::endl;
+std::cout<<c<<std::endl;
+
 
     return 0;
 }
