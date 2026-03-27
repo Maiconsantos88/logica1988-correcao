2e8235daaeefc6dc528ac3adba6be6dc1f3cb053
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-03-27 20:19:21 -0300

    Atualizado2

--- main1.cpp
+++ main1.cpp
@@ -3,15 +3,10 @@
 
 int main(){
 
-int a = 8;
-float b = 9.6;
-double c = 8.695136784;
-
-std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
-
-std::cout<<a<<std::endl;
-std::cout<<b<<std::endl;
-std::cout<<c<<std::endl;
+    std::cout<<8+2<<std::endl;
+    std::cout<<10-4<<std::endl;
+    std::cout<<4*3<<std::endl;
+    std::cout<<8/2<<std::endl;
 
 
     return 0;
