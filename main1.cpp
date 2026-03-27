d05b4d2610c79fb352720e9cc552dac8aa2f9b1c
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-03-27 20:36:44 -0300

    Atualizado 4

--- main1.cpp
+++ main1.cpp
@@ -5,11 +5,17 @@ int main(){
 
     int num1 = 8;
     int num2 = 2;
+    int soma, sub, mult, div;
 
-    std::cout<<num1 + num2<<std::endl;
-    std::cout<<num1 - num2<<std::endl;
-    std::cout<<num1 * num2<<std::endl;
-    std::cout<<num1 / num2<<std::endl;
-    
+   soma = num1 + num2;
+   sub = num1 - num2;
+   mult = num1 * num2;
+   div = num1 / num2;
+
+    std::cout<<soma<<std::endl;
+    std::cout<<sub<<std::endl;
+    std::cout<<mult<<std::endl;
+    std::cout<<div<<std::endl;
+
     return 0;
 }
