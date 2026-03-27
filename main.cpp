fc8b26eda9b21c18e4d8da62a47c0388ff493593
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-03-26 22:09:04 -0300

    teste

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+
+int main(){
+
+    std::string saudacao = "Boa noite, Eduardo ";
+    //comentário de teste
+    std::cout<<saudacao<<std::endl;
+
+    return 0;
+}
