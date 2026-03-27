af569a9002ea16f6723afcfccf8244c0f1ef33ef
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-03-27 20:44:35 -0300

    Correção Variavel subt

--- main1.cpp
+++ main1.cpp
@@ -5,15 +5,15 @@ int main(){
 
     int num1 = 8;
     int num2 = 2;
-    int soma, sub, mult, div;
+    int soma, subt, mult, div;
 
    soma = num1 + num2;
-   sub = num1 - num2;
+   subt = num1 - num2;
    mult = num1 * num2;
    div = num1 / num2;
 
     std::cout<<soma<<std::endl;
-    std::cout<<sub<<std::endl;
+    std::cout<<subt<<std::endl;
     std::cout<<mult<<std::endl;
     std::cout<<div<<std::endl;
 
