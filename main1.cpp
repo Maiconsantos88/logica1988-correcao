0981a9f45a84f50e9e81607f305d937e4cbdb468
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-01 19:32:49 -0300

    adiciona uso de variavel char para armazenar e exibir um caractere

--- main1.cpp
+++ main1.cpp
@@ -3,14 +3,11 @@
 
 int main(){
 
-    bool condicao = true;
-    bool condicao1 = false;
-
-    std::cout<<condicao<<std::endl;
-    std::cout<<condicao1<<std::endl;
-   
+    char letra = 'E';
 
 
+    std::cout<<letra<<std::endl;
+    
    
     return 0;
 }
