2f36025517138aa2d0a7831abf0477fb2321fdfb
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-01 21:55:55 -0300

    feat: adiciona concatenacao de strings para exibir nome completo

--- main1.cpp
+++ main1.cpp
@@ -3,26 +3,10 @@
 
 int main(){
 
-    int num1, num2, soma;
+   std::string nome = "Eduardo";
+   std::string sobrenome = "Barreto";
 
-    std::cout<<"Digite num 1: "<<"\n";
-    std::cin>>num1;
-
-    std::cout<<"Digite num 2: "<<"\n";
-    std::cin>>num2;
-
-    soma = num1 + num2;
-
-    std::cout<<"O resultado da soma é "<<soma<<"\n";
-
-    //New Line: Apenas insere o caractere de nova linha.
-    //É como apertar o enter em umeditor de texto. O computador
-    //guarda essa informação em um espaço temporário (chamado buffer) e só mostra
-    //na tela quando esse espaço enche ou o programa acaba.
-
-    // std::endl Insere a nova linha e força o esvaziamento do buffer (operação chama de flush). Ele
-    // obriga o computador a exibir tudo o que está guardado na memória imediatamente na tela.
-
+   std::cout<<nome + " " + sobrenome<<std::endl;
 
     return 0;
 }
