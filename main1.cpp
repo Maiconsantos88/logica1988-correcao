825958895c8ccbe689e39f3df52df4971aec7bba
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-01 21:07:52 -0300

    adiciona exemplos de precedencia de operadores em expressoes matematicas

--- main1.cpp
+++ main1.cpp
@@ -1,30 +1,14 @@
 #include<iostream> // Biblioteca para entrada e saída (cout e cin)
-#include<string>
+
 
 int main(){
 
-   //Solicite ao usuário para ele digitar nome (primeiro nome)
-   //Colocar a idade
-   //fazer um cáculo para que diga que ano nasceu
-   //No final mostre o nome da pessoa.. você nasceu no ano...
-   //Vamos tentar SEM IA??
+    int result1 = 4 + 4 * 2;
+    int result2 = 4 + 2 / 2;
 
-   std::string nome;
-   int idade;
-   int anoAtual = 2026;
-   int anoNascimento;
+    std::cout<<result1<<std::endl;
+    std::cout<<result2<<std::endl;
 
-    std::cout<<"Qual seu nome:"<<std::endl;
-    std::cin>>nome;
-    std::cout<<"Qual sua idade:"<<std::endl;
-    std::cin>>idade;
-
-    anoNascimento = anoAtual - idade;
-
-    std::cout << "Bem-vindo(a), " << nome <<"!!"<<std::endl;
-    std::cout<< "Você nasceu em: "<<anoNascimento<<"."<<std::endl;
-
- 
     return 0;
 }
 
