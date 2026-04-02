865bb7c7b02a6416fbdaff6c55f95cd2845c34ce
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-01 21:36:58 -0300

    docs: adiciona explicacao sobre uso de \\n e std::endl no codigo

--- main1.cpp
+++ main1.cpp
@@ -3,13 +3,26 @@
 
 int main(){
 
-    int result1 = 4 + 4 * 2;
-    int result2 = 4 + 2 / 2;
-    int result3 = (4 + 4) * 2;
+    int num1, num2, soma;
 
-    std::cout<<result1<<std::endl;
-    std::cout<<result2<<std::endl;
-    std::cout<<result3<<std::endl;
+    std::cout<<"Digite num 1: "<<"\n";
+    std::cin>>num1;
+
+    std::cout<<"Digite num 2: "<<"\n";
+    std::cin>>num2;
+
+    soma = num1 + num2;
+
+    std::cout<<"O resultado da soma é "<<soma<<"\n";
+
+    //New Line: Apenas insere o caractere de nova linha.
+    //É como apertar o enter em umeditor de texto. O computador
+    //guarda essa informação em um espaço temporário (chamado buffer) e só mostra
+    //na tela quando esse espaço enche ou o programa acaba.
+
+    // std::endl Insere a nova linha e força o esvaziamento do buffer (operação chama de flush). Ele
+    // obriga o computador a exibir tudo o que está guardado na memória imediatamente na tela.
+
 
     return 0;
 }
