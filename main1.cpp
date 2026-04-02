314947c47f202ee47c875a0036a478e793708ea2
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-01 21:13:34 -0300

    adiciona uso de parenteses para alterar precedencia das operacoes

--- main1.cpp
+++ main1.cpp
@@ -5,9 +5,11 @@ int main(){
 
     int result1 = 4 + 4 * 2;
     int result2 = 4 + 2 / 2;
+    int result3 = (4 + 4) * 2;
 
     std::cout<<result1<<std::endl;
     std::cout<<result2<<std::endl;
+    std::cout<<result3<<std::endl;
 
     return 0;
 }
