92db070aa4aad36e3965ec7c0edf128ef5e24947
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-06 20:23:27 -0300

    feat: adiciona manipulacao de string utilizando append

--- main1.cpp
+++ main1.cpp
@@ -1,13 +1,16 @@
 #include<iostream> // Biblioteca para entrada e saída (cout e cin)
-
+#include<string>
 
 int main(){
 
-   std::string nome = "Eduardo";
-   std::string sobrenome = "Barreto";
+    std::string nome;
+    std::string sobrenome = " Barreto";
 
-   std::cout<<nome + " " + sobrenome<<std::endl;
+    std::cout<<"Digite seu nome: "<<std::endl;
+    std::cin>>nome;
 
+    std::cout<<nome.append(sobrenome);
+   
     return 0;
 }
 
