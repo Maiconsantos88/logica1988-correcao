92f813eacae9a18b66214c16131521cd83b74901
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-06 20:45:26 -0300

    fix: corrige erro de incompatibilidade de tipos entre int e string

--- main1.cpp
+++ main1.cpp
@@ -4,13 +4,11 @@
 int main(){
 
     int a = 40;
-    int b = 10;
 
-    std::string c = "40";
     std::string d = "10";
 
-    std::cout<<a+b<<std::endl;
-    std::cout<<c+d<<std::endl;
+    std::cout<<a+d<<std::endl;
+    
     
    
     return 0;
