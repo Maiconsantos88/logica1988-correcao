4a68e601cef9206d390f30197386e3670760789a
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-06 21:55:21 -0300

    acessa e altera caracteres de string

--- main1.cpp
+++ main1.cpp
@@ -5,7 +5,11 @@ int main(){
 
    std::string nome = "Maicon";
 
-   std::cout<<nome[0];
+   std::cout<<nome[0]<<std::endl;
+   std::cout<<nome[2]<<std::endl;
+   std::cout<<nome[nome.length() -1]<<std::endl;
+   nome[0] = 'A';
+   std::cout<<nome<<std::endl;
 
       
     return 0;
