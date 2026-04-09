66da439d131951e365c00ce9e5671f4edb6b649d
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-08 21:54:34 -0300

    feat: sistema de login com validação de usuario e senha

--- main1.cpp
+++ main1.cpp
@@ -1,16 +1,22 @@
 #include<iostream> // Biblioteca para entrada e saída (cout e cin)
-
+#include<string> 
 
 int main(){  // Função principal onde o programa começa
 
-    int a = 5;
-    int b = 2;
+   std::string user;
+   std::string senha;
 
-    if(a > b){
-        std::cout<<"A é maior que B."<<std::endl;
-    }else{
-        std::cout<<"B é maior ou igual a A."<<std::endl;
-    }
+   std::cout<<"Digite seu usuario de acesso: "<<std::endl;
+   std::cin>>user;
+
+   std::cout<<"Digite sua senha: "<<std::endl;
+   std::cin>>senha;
+
+   if(senha == "123456789"){
+    std::cout<<"O usuario "<<user<<" está logado com sucesso!!"<<std::endl;
+   }else{
+    std::cout<<"Usuario ou senha invalidos!"<<std::endl;
+   }
 
     return 0;
 }
