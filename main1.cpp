2864a65ca0ff44176e9afb3642797237d34e1e35
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-13 19:48:53 -0300

    feat: implementa faixas etárias (bebê, criança, adolescente, adulto, sênior)

--- main1.cpp
+++ main1.cpp
@@ -8,9 +8,17 @@ int main(){  // Função principal onde o programa começa
    std::cin>>idade;
 
    if(idade < 0){
-    std::cout<<"Idade invalida!"<<std::endl;
+        std::cout<<"Idade invalida!"<<std::endl;
    }else if(idade <= 2){
-    std::cout<<"Voce e um bebe!"<<std::endl;
+        std::cout<<"Voce e um bebe!"<<std::endl;
+   }else if(idade <= 8){
+        std::cout<<"voce e uma crianca!"<<std::endl;
+   }else if(idade <= 16){
+        std::cout<<"voce e um(a) adolescente!"<<std::endl;
+   }else if(idade <= 60){
+        std::cout<<"voce e adulto(a)!"<<std::endl;
+   }else{
+        std::cout<<"voce e um(a) senior!"<<std::endl;
    }
 
     return 0;
