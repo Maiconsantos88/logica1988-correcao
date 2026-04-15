8e3100abbcec08e3500809c1665a98c8bf69d338
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-15 19:18:43 -0300

    feat: implementa loop for exibindo mensagem 10 vezes

--- main1.cpp
+++ main1.cpp
@@ -1,33 +1,14 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 
-int main(){  // Função principal onde o programa começa
+int main(){ // Função principal onde o programa começa
 
-   std::string nome;
-   float altura;
-   int idade;
+     for(int i = 1; i <= 10; i++){
+          std::cout<<"Este é um loop"<<std::endl;
+     }
 
-   std::cout<<"Digite seu nome: "<<std::endl;
-   std::cin>>nome;
+     
 
-   std::cout<<"Digite sua altura: "<<std::endl;
-   std::cin>>altura;
+    
 
-   std::cout<<"Digite sua idade: "<<std::endl;
-   std::cin>>idade;
-
-   if(idade < 0){
-        std::cout<<"Voce disgitou um valor invalido para uma idade!"<<std::endl;
-   }else if(idade <= 10){
-        std::cout<<"Voce e uma crianca!"<<std::endl;
-   }else if(idade <= 18){
-        std::cout<<"voce e um(a) adolescente!"<<std::endl;
-   }else if(idade <= 60){
-        std::cout<<"voce e adulto(a)!"<<std::endl;
-   }else{
-        std::cout<<"voce e um(a) senior!"<<std::endl;
-   }
-
-
-    return 0;
+     return 0;
 }
