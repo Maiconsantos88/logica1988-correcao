7457b4aeda97f86d6d9fbfe54756f085311174d6
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-15 21:21:49 -0300

    feat: implementa loops aninhados para demonstrar execução de laço interno dentro do externo

--- main1.cpp
+++ main1.cpp
@@ -6,13 +6,14 @@ int main(){ // Função principal onde o programa começa
      SetConsoleOutputCP(CP_UTF8);
      SetConsoleCP(CP_UTF8);
 
-     int soma = 0;
+     for(int i = 1; i <= 2; ++i){
+          std::cout<<"Externo: "<<i<<std::endl;
 
-     for(int i = 1; i <= 5; i++){
-          soma = soma + i;
+          for(int j = 1; j <= 3; ++j){
+               std::cout<<"Interno: "<<j<<std::endl;
+          }
+
      }
-
-    std::cout<<"Soma é: "<<soma<<std::endl;
     
      return 0;
 }
