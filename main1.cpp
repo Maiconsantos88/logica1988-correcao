14c120e82a39225cfdc7fe5b977cd65d997b63fe
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-15 21:00:35 -0300

    feat: calcula soma de 1 a 5 com for

--- main1.cpp
+++ main1.cpp
@@ -6,43 +6,13 @@ int main(){ // Função principal onde o programa começa
      SetConsoleOutputCP(CP_UTF8);
      SetConsoleCP(CP_UTF8);
 
-     int opcao;
+     int soma = 0;
 
-     std::cout<<"Digite um valor para opção: "<<std::endl;
-     std::cin>>opcao;
-
-     switch(opcao){
-          case 1:
-               std::cout<<"Domingo!"<<std::endl;
-               break;
-          case 2:
-               std::cout<<"Segunda-Feira!"<<std::endl;
-               break;
-          case 3:
-               std::cout<<"Terça-Feira!"<<std::endl;
-               break;
-          case 4:
-               std::cout<<"Quarta-Feira!"<<std::endl;
-               break;
-          case 5:
-               std::cout<<"Quinta-Feira!"<<std::endl;
-               break;
-          case 6:
-               std::cout<<"Sexta-Feira!"<<std::endl;
-               break;
-          case 7:
-               std::cout<<"Sábado!"<<std::endl;
-               break;
-          default:
-               std::cout<<"Número inválido!"<<std::endl;
-
-
-
+     for(int i = 1; i <= 5; i++){
+          soma = soma + i;
      }
 
-     
-     
-     
-
+    std::cout<<"Soma é: "<<soma<<std::endl;
+    
      return 0;
 }
