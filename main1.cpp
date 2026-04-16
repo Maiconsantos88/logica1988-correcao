94e944838d80c4a9309461347d174962874c6b33
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-15 21:43:22 -0300

    feat: loop com índice em array

--- main1.cpp
+++ main1.cpp
@@ -7,8 +7,11 @@ int main(){ // Função principal onde o programa começa
      SetConsoleCP(CP_UTF8);
 
      int myNumbers[5] = {10, 20, 30, 40, 50};
-     for(int num : myNumbers){
-          std::cout<<num<<std::endl;
+     // for(int num : myNumbers){
+     //      std::cout<<num<<std::endl;
+
+     for(int i = 0; i < 5; ++i){
+          std::cout<<myNumbers[i]<<std::endl;
      }
      
 
