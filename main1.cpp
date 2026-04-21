87c34a3b7b8550aa555adbf0c854e7533ed0b652
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-20 21:49:30 -0300

    feat: implementa validação de número com loop do-while e mensagem de erro

--- main1.cpp
+++ main1.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <windows.h>
 
-int main()
-{ // Função principal onde o programa começa
+int main(){ // Função principal onde o programa começa
 
      SetConsoleOutputCP(CP_UTF8);
      SetConsoleCP(CP_UTF8);
 
-     int contagem = 10;
+     int num;
 
-     while (contagem > 0){
-          std::cout<<contagem<<std::endl;
-          contagem--;
-     }
+     do{
+          std::cout << "Digite um valor para num:" <<std::endl;
+          std::cin >> num;
 
-     std::cout<<"Feliz ano novo!!"<<std::endl;
+          if (num < 1 || num > 10){
+               std::cout << "Entrada inválida!!" << std::endl;
+          }
+
+     }while (num < 1 || num > 10);
+          {
+               std::cout << "Obrigado!! Você escolheu o número:" <<num<< std::endl;
+          }
+     
 
      return 0;
 }
