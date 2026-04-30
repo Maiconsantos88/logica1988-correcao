fbbae6d3fe9078071dc19d2c2d0a651b5d472e50
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-30 19:23:29 -0300

    feat: cria programa de cadastro com nome e idade

--- main1.cpp
+++ main1.cpp
@@ -1,21 +1,30 @@
-#include <iostream>
-
+#include<iostream>
+#include<string>
 #include <windows.h>
-
-int doubleGame(int x){
-     return x * 2;
-}
-
-     
- 
 
 int main(){ 
 
      SetConsoleOutputCP(CP_UTF8);
      SetConsoleCP(CP_UTF8);
 
-     for(int i = 1; i <= 5; i++){
-          std::cout<<"O dobro de "<<i<<" é "<<doubleGame(i)<<std::endl;
+     int opcao;
+     std::string nome;
+     int idade;
+
+     std::cout<<"----- Escolha a opção adequada: "<<std::endl;
+     std::cout<<"Digite 1 para cadastrar ou 2 para sair: "<<std::endl;
+     std::cin>>opcao;
+
+     if(opcao == 1){
+          std::cout<<"Digite o nome: "<<std::endl;
+          std::cin>>nome;
+          std::cout<<"Digite a idade: "<<std::endl;
+          std::cin>>idade;
+
+          std::cout<<"Seu nome é: "<<nome<<std::endl;
+          std::cout<<"Sua idade é: "<<idade<<std::endl;
+     }else{
+          std::cout<<"Até a próxima!"<<std::endl;
      }
    
      return 0;
