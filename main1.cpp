69aa9b4ef24dfe1c94dd8a1ff55bb659e4c8b074
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-04-30 20:52:23 -0300

    feat: verifica se número é par ou ímpar

--- main1.cpp
+++ main1.cpp
@@ -6,29 +6,18 @@ int main(){
      SetConsoleOutputCP(CP_UTF8);
      SetConsoleCP(CP_UTF8);
 
-     //brlAmount: valor em reais | exchangeRate: Taxa de Câmbio | usdAmount: Valor em dólares
-     double brlAmount, exchangeRate, usdAmount;
-     int choice; //choice: escolha
+     int num;
 
-     std::cout<<"----- Currency Converter: BRL to USD ----- "<<std::endl;
+     std::cout<<"Digite um valor para num: "<<std::endl;
+     std::cin>>num;
+
+     if(num % 2 == 0){
+        std::cout<<num<<"É par!"<<std::endl;
+    }else{
+        std::cout<<num<<"É impar!!"<<std::endl;
+    }
+
      
-     //Entrada da taxa de câmbio (quanto vale 1 dolar hoje)
-     std::cout<<"Enter the current exchange rate (How much is 1 dollar in Reais?): "<<std::endl;
-     std::cin>>exchangeRate;
-
-     //Entrada da quantia em Reais que a pessoa deseja converter
-     std::cout<<"Enter the amount in BRL(R$): "<<std::endl;
-     std::cin>>brlAmount;
-
-     //Verificação Lógica: garante que o valor informado seja positivo
-     if(brlAmount > 0){
-          //cálculo: divide o montante em reais pela taxa de câmbio
-          usdAmount = brlAmount / exchangeRate;
-          std::cout<<"You will have $"<<usdAmount<<" US Dollars."<<std::endl;
-     }else{
-          //Mensagem de erro caso o valor seja zero ou negativo
-          std::cout<<"Error: Please enter a valid amount greater than zero."<<std::endl;
-     } 
 
 
 
