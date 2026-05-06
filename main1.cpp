8483255c032d861e1181f6295ca5d0106caa95e4
Maiconsantos88 <01376703033@senacrs.edu.br>
2026-05-05 22:06:11 -0300

    feat: adiciona sistema inicial de matrícula com escolha de modalidade e entrada de dados do aluno

--- main1.cpp
+++ main1.cpp
@@ -1,32 +1,47 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 #include <windows.h>
 
-int main(){ 
+void exibirModalidade(){
+     std::cout<<"\n========== SENAC NH - MATRICULAS =========="<<std::endl;
+     std::cout<<"1 - Cursos presenciais"<<std::endl;
+     std::cout<<"2 - Cursos EAD"<<std::endl;
+     std::cout<<"3 - Idiomas (Libras)"<<std::endl;
+     std::cout<<"0 - Sair do Sistema"<<std::endl;
+     std::cout<<"Selecione a modalidade do curso: "<<std::endl;
+}
 
+int main()
+{
      SetConsoleOutputCP(CP_UTF8);
      SetConsoleCP(CP_UTF8);
 
-    std::string nomeCompleto, cpf, estado, matricula;
-    std::string cpf , cpfResponsavelFinanceiro
-    std::string rua, numeroCasa;
-    std::string telefoneAluno, telefoneMae, telefonePai;
-    std::string curso;
+     int modalidade = -1;
 
+     exibirModalidade();
+     modalidade;
 
      
-     
-     std::cout<<"Digite o nome completo do aluno: "<<std::endl;
+
+     std::string nomeCompleto, nomeCompletoResponsavelFinaceiro, estado, matricula;
+     std::string emailAluno, emailResponsavelFinanceiro;
+     std::string cpf, cpfResponsavelFinanceiro;
+     std::string rgAluno, rgResponsavelFinanceiro;
+     std::string enderecoCompletoAluno, enderecoResponsavelFinaceiro, numeroCasa, cepAluno, cepResponsavelFinaceiro;
+     std::string contatoAluno, contatoResponsavelFinaceiro;
+     std::string curso;
+     int idade;
+     double valorCurso, valorFinal, valorParcela;
+     int opcaoPagamento;
+
+     std::cout << "Digite o nome completo do aluno: " << std::endl;
      std::getline(std::cin, nomeCompleto);
 
-     std::cout<<"Digite o cpf: "<<std::endl;
+     std::cout << "Digite o cpf: " << std::endl;
      std::getline(std::cin, cpf);
 
-     std::cout<<nomeCompleto<<std::endl;
-     std::cout<<cpf<<std::endl;
-     
-
+     std::cout << nomeCompleto << std::endl;
+     std::cout << cpf << std::endl;
 
      return 0;
-
 }
